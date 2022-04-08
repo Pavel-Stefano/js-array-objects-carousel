87dc459aa10289d54a733ea6bcf1798a4f7a0798
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-04-08 15:27:58 +0200

    struttura oggetti

---
+++ js/mio.js
@@ -0,0 +1,32 @@
+const slider = [
+    {
+      immagine:"img/01.jpg",
+      titolo:"Svezia",
+      descrizione:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+    {
+      immagine:"img/02.jpg",
+      titolo:"Svizzera",
+      descrizione:"Lorem ipsum",
+    },
+    {
+      immagine:"img/03.jpg",
+      titolo:"Gran Bretagna",
+      descrizione:"Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+  
+    },
+    {
+      immagine:"img/04.jpg",
+      titolo:"Germania",
+      descrizione:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+    },
+    {
+      immagine:"img/05.jpg",
+      titolo: "Paradise",
+      descrizione:"Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+  
+    },
+  ];
+
+  
+  
